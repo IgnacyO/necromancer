2c46a401c10ec888f06bcefe6747fe710c834aaf
IgnacyOziero <oziero.ignacy@protonmail.com>
2026-04-23 09:18:38 +0200

    feat: add nec transmitter example

--- main/main.c
+++ main/main.c
@@ -15,12 +15,14 @@
 #include "freertos/projdefs.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
+#include "ir_nec_encoder.h"
 #include "ir_nec_parser.h"
 #include "driver/gpio.h"
 
 #define IR_RX_GPIO GPIO_NUM_27
+#define IR_TX_GPIO GPIO_NUM_26
 #define IR_NEC_RESOLUTION_HZ 1000000 // 1MHz resolution, 1 tick = 1us
-
+#define TAG "Example"
 
 void ir_nec_init(rmt_channel_handle_t *rmt_rx_chan) {
 	static const rmt_rx_channel_config_t rmt_rx_chan_config = {
@@ -72,9 +74,7 @@ void parse_received_symbols_to_nec(rmt_symbol_word_t *rmt_symbols, size_t num_sy
 void app_main(void)
 {
 	rmt_channel_handle_t rx_channel;
-	ir_nec_init(&rx_channel);
-	ESP_ERROR_CHECK(rmt_enable(rx_channel));
-	
+	ir_nec_init(&rx_channel);	
 	rmt_symbol_word_t raw_symbols[64];
 	rmt_rx_done_event_data_t rx_data;
 	QueueHandle_t receive_queue = xQueueCreate(1, sizeof(rmt_rx_done_event_data_t));
@@ -89,6 +89,38 @@ void app_main(void)
 	    .signal_range_max_ns = 12000000, // the longest duration for NEC signal is 9000us, 12000000ns > 9000us, the receive won't stop early
 	};
 	
+	 rmt_tx_channel_config_t tx_channel_cfg = {
+        .clk_src = RMT_CLK_SRC_DEFAULT,
+        .resolution_hz = IR_NEC_RESOLUTION_HZ,
+        .mem_block_symbols = 64, // amount of RMT symbols that the channel can store at a time
+        .trans_queue_depth = 4,  // number of transactions that allowed to pending in the background, this example won't queue multiple transactions, so queue depth > 1 is sufficient
+        .gpio_num = IR_TX_GPIO,
+    };
+    rmt_channel_handle_t tx_channel = NULL;
+    ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_channel_cfg, &tx_channel));
+
+    rmt_carrier_config_t carrier_cfg = {
+        .duty_cycle = 0.33,
+        .frequency_hz = 38000, // 38KHz
+    };
+    ESP_ERROR_CHECK(rmt_apply_carrier(tx_channel, &carrier_cfg));
+
+    // this example won't send NEC frames in a loop
+    rmt_transmit_config_t transmit_config = {
+        .loop_count = 0, // no loop
+    };
+
+    ESP_LOGI(TAG, "install IR NEC encoder");
+    ir_nec_encoder_config_t nec_encoder_cfg = {
+        .resolution = IR_NEC_RESOLUTION_HZ,
+    };
+    rmt_encoder_handle_t nec_encoder = NULL;
+    ESP_ERROR_CHECK(rmt_new_ir_nec_encoder(&nec_encoder_cfg, &nec_encoder));
+
+    ESP_LOGI(TAG, "enable RMT TX and RX channels");
+    ESP_ERROR_CHECK(rmt_enable(tx_channel));
+    ESP_ERROR_CHECK(rmt_enable(rx_channel));
+	
 	ESP_ERROR_CHECK(rmt_receive(rx_channel, raw_symbols, sizeof(raw_symbols), &receive_config));
 	
 	
@@ -98,6 +130,12 @@ void app_main(void)
         	parse_received_symbols_to_nec(rx_data.received_symbols, rx_data.num_symbols);
             // start receive again
             ESP_ERROR_CHECK(rmt_receive(rx_channel, raw_symbols, sizeof(raw_symbols), &receive_config));
-        } 
+        } else {
+			const ir_nec_scan_code_t scan_code = {
+				.address = 0x0440,
+				.command = 0x3003
+			};
+			ESP_ERROR_CHECK(rmt_transmit(tx_channel, nec_encoder, &scan_code, sizeof(scan_code), &transmit_config));
+		}
     }
 }
