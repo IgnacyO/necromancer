a7f68809bbee3e1bb8b524190df394283afa95a2
IgnacyOziero <oziero.ignacy@protonmail.com>
2026-06-26 14:12:28 +0200

    feat/refactor: add/update logging messages for ir nec module

--- components/ir_nec/ir_nec.c
+++ components/ir_nec/ir_nec.c
@@ -1,24 +1,25 @@
 #include "ir_nec.h"
-#include <string.h>
+#include "common.h"
 #include "driver/rmt_common.h"
 #include "driver/rmt_encoder.h"
 #include "driver/rmt_types.h"
 #include "esp_log.h"
+#include "events.h"
 #include "freertos/projdefs.h"
-#include "freertos/ringbuf.h"
 #include "freertos/queue.h"
+#include "freertos/ringbuf.h"
 #include "hal/rmt_types.h"
 #include "ir_nec_encoder.h"
 #include "ir_nec_parser.h"
-#include "events.h"
+#include <string.h>
 
 #define RX_SYMBOL_NUM 128
 #define RMT_BLOCK_SYMBOLS_NUM 128
 
-static const char* IR_NEC_TAG = "ir_nec";
+static const char *IR_NEC_TAG = "IR NEC";
 
 static const rmt_receive_config_t receive_config = {
-    .signal_range_min_ns = IR_NEC_SIG_RANGE_MIN_NS,     
+    .signal_range_min_ns = IR_NEC_SIG_RANGE_MIN_NS,
     .signal_range_max_ns = IR_NEC_SIG_RANGE_MAX_NS,
 };
 
@@ -27,202 +28,213 @@ static const rmt_transmit_config_t transmit_config = {
 };
 
 static void ir_nec_tx_task(void *pvParameters) {
-	task_arg_t *arg = pvParameters; 
-	ir_nec_t *ir_nec = arg->dev_ptr;
-	size_t received_frame_len;
-	nec_scan_code_t *frame_to_be_sent;
-	for (;;) {
-		if ((frame_to_be_sent = (nec_scan_code_t*)xRingbufferReceive(ir_nec->tx_interm_buf, &received_frame_len, 10)) != NULL) {
-			ESP_LOGI(IR_NEC_TAG, "GOT SOMETHING TX %d %d\n", frame_to_be_sent->address, frame_to_be_sent->command);
-			if (rmt_transmit(ir_nec->rmt_tx_chan, ir_nec->nec_encoder, frame_to_be_sent, sizeof(nec_scan_code_t), &transmit_config) != ESP_OK){
-				ESP_LOGE(IR_NEC_TAG, "Error while transmitting");	
-			}
-			vRingbufferReturnItem(ir_nec->tx_interm_buf, frame_to_be_sent);
-		}
-		else {
-			vTaskDelay(pdMS_TO_TICKS(10));
-		}
-	}
-	vTaskDelete(NULL);
+  task_arg_t *arg = pvParameters;
+  ir_nec_t *ir_nec = arg->dev_ptr;
+  size_t received_frame_len;
+  nec_scan_code_t *frame_to_be_sent;
+  for (;;) {
+    if ((frame_to_be_sent = (nec_scan_code_t *)xRingbufferReceive(
+             ir_nec->tx_interm_buf, &received_frame_len, 10)) != NULL) {
+      if (g_debug_enabled) {
+        ESP_LOGD(IR_NEC_TAG, "Sending nec frame: A: %x C: %x",
+                 frame_to_be_sent->address, frame_to_be_sent->command);
+      }
+      if (rmt_transmit(ir_nec->rmt_tx_chan, ir_nec->nec_encoder,
+                       frame_to_be_sent, sizeof(nec_scan_code_t),
+                       &transmit_config) != ESP_OK) {
+        ESP_LOGE(IR_NEC_TAG, "Error occured while transmitting.");
+      }
+      vRingbufferReturnItem(ir_nec->tx_interm_buf, frame_to_be_sent);
+    } else {
+      vTaskDelay(pdMS_TO_TICKS(10));
+    }
+  }
+  vTaskDelete(NULL);
 }
 
 static void ir_nec_rx_task(void *pvParameters) {
-    task_arg_t *arg = pvParameters; 
-	ir_nec_t *ir_nec = arg->dev_ptr;
+  task_arg_t *arg = pvParameters;
+  ir_nec_t *ir_nec = arg->dev_ptr;
 
-    rmt_rx_done_event_data_t evt_data;
+  rmt_rx_done_event_data_t evt_data;
 
-    static rmt_symbol_word_t rx_symbols[RX_SYMBOL_NUM];
-    			ESP_LOGI(IR_NEC_TAG, "IR RX TASK RUNNING");
+  static rmt_symbol_word_t rx_symbols[RX_SYMBOL_NUM];
 
+  ESP_ERROR_CHECK(rmt_receive(ir_nec->rmt_rx_chan, rx_symbols,
+                              sizeof(rx_symbols), &receive_config));
 
-    ESP_ERROR_CHECK(
-        rmt_receive(
-            ir_nec->rmt_rx_chan,
-            rx_symbols,
-            sizeof(rx_symbols),
-            &receive_config
-        )
-    );
+  for (;;) {
 
-    for (;;) {
+    if (xQueueReceive(ir_nec->rmt_rx_queue, &evt_data, 10) == pdTRUE) {
+      nec_scan_code_t scan_code = {0, 0};
 
-        if (xQueueReceive(
-                ir_nec->rmt_rx_queue,
-                &evt_data,
-                10
-            ) == pdTRUE) {
-			ESP_LOGI(IR_NEC_TAG, "GOT SOMETHING FROM IR RX");
-            nec_scan_code_t scan_code = {0, 0};
-			
-            if (parse_received_symbols_to_nec(evt_data.received_symbols, evt_data.num_symbols, ir_nec->error_correction,&scan_code)) {
-               
-				xRingbufferSend(ir_nec->rx_interm_buf,
-				                    &scan_code,
-				                    sizeof(scan_code),
-				                    10);
+      if (parse_received_symbols_to_nec(evt_data.received_symbols,
+                                        evt_data.num_symbols,
+                                        ir_nec->error_correction, &scan_code)) {
 
-            }
-            else {
-            	ESP_LOGE(IR_NEC_TAG, "Error parsing NEC");
-            }
+        if (g_debug_enabled)
+          ESP_LOGD(IR_NEC_TAG, "Received nec frame: A: %x C: %x",
+                   scan_code.address, scan_code.command);
+        xRingbufferSend(ir_nec->rx_interm_buf, &scan_code, sizeof(scan_code),
+                        10);
 
-            ESP_ERROR_CHECK(
-                rmt_receive(
-                    ir_nec->rmt_rx_chan,
-                    rx_symbols,
-                    sizeof(rx_symbols),
-                    &receive_config
-                )
-            );
-        }
-        else {
-			vTaskDelay(pdMS_TO_TICKS(10));
-		}
+      } else {
+        ESP_LOGE(IR_NEC_TAG, "Error while parsing received nec frame.");
+      }
+
+      ESP_ERROR_CHECK(rmt_receive(ir_nec->rmt_rx_chan, rx_symbols,
+                                  sizeof(rx_symbols), &receive_config));
+    } else {
+      vTaskDelay(pdMS_TO_TICKS(10));
     }
-    vTaskDelete(NULL);
+  }
+  vTaskDelete(NULL);
 }
 
-static bool rmt_rx_done_callback(rmt_channel_handle_t channel, const rmt_rx_done_event_data_t *event_data, void *user_data) {
-	BaseType_t high_task_wakeup = pdFALSE;
-	QueueHandle_t rx_queue = *(QueueHandle_t*)user_data;
-	xQueueSendFromISR(rx_queue, event_data, &high_task_wakeup);
-	ESP_EARLY_LOGI("RMT", "RX DONE");
-	return high_task_wakeup == pdTRUE;
+static bool rmt_rx_done_callback(rmt_channel_handle_t channel,
+                                 const rmt_rx_done_event_data_t *event_data,
+                                 void *user_data) {
+  BaseType_t high_task_wakeup = pdFALSE;
+  QueueHandle_t rx_queue = *(QueueHandle_t *)user_data;
+  xQueueSendFromISR(rx_queue, event_data, &high_task_wakeup);
+  return high_task_wakeup == pdTRUE;
 }
 
 esp_err_t ir_nec_init(ir_nec_t *ir_nec, const ir_nec_config_t *config) {
-    ir_nec->initialized = true;
-    ir_nec->rx_running = false;
-    ir_nec->tx_running = false;
-    ir_nec->error_correction = config->error_correction;
-    ir_nec->rx_interm_buf = xRingbufferCreate(config->rx_interm_buf_sz, RINGBUF_TYPE_NOSPLIT);
-    ir_nec->tx_interm_buf = xRingbufferCreate(config->tx_interm_buf_sz, RINGBUF_TYPE_NOSPLIT);
-    ir_nec->rmt_rx_queue = xQueueCreate(config->rmt_rx_queue_size, sizeof(rmt_rx_done_event_data_t));
+  ESP_LOGI(IR_NEC_TAG, "Initializing module...");
+  ir_nec->initialized = true;
+  ir_nec->rx_running = false;
+  ir_nec->tx_running = false;
+  ir_nec->error_correction = config->error_correction;
+  ir_nec->rx_interm_buf =
+      xRingbufferCreate(config->rx_interm_buf_sz, RINGBUF_TYPE_NOSPLIT);
+  ir_nec->tx_interm_buf =
+      xRingbufferCreate(config->tx_interm_buf_sz, RINGBUF_TYPE_NOSPLIT);
+  ir_nec->rmt_rx_queue =
+      xQueueCreate(config->rmt_rx_queue_size, sizeof(rmt_rx_done_event_data_t));
 
-	rmt_rx_channel_config_t rmt_rx_chan_config = {
-		.clk_src = RMT_CLK_SRC_DEFAULT,
-		.gpio_num = config->rx_gpio,
-		.resolution_hz = config->resolution_hz,
-		.mem_block_symbols = RMT_BLOCK_SYMBOLS_NUM,
-		.flags.invert_in = config->invert_in
-	};
-	ESP_ERROR_CHECK(rmt_new_rx_channel(&rmt_rx_chan_config, &ir_nec->rmt_rx_chan));
-    
-    rmt_rx_event_callbacks_t cbs = {
-        .on_recv_done = rmt_rx_done_callback,
-    };
-    ESP_ERROR_CHECK(rmt_rx_register_event_callbacks(ir_nec->rmt_rx_chan, &cbs, &ir_nec->rmt_rx_queue));
-	
-	rmt_tx_channel_config_t tx_channel_cfg = {
-        .clk_src = RMT_CLK_SRC_DEFAULT,
-        .resolution_hz = config->resolution_hz,
-        .mem_block_symbols = RMT_BLOCK_SYMBOLS_NUM,
-        .trans_queue_depth = 4,
-        .gpio_num = config->tx_gpio,
-    };
-    ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_channel_cfg, &ir_nec->rmt_tx_chan));
-    ESP_ERROR_CHECK(rmt_apply_carrier(ir_nec->rmt_tx_chan, &config->tx_carrier_config));
+  rmt_rx_channel_config_t rmt_rx_chan_config = {
+      .clk_src = RMT_CLK_SRC_DEFAULT,
+      .gpio_num = config->rx_gpio,
+      .resolution_hz = config->resolution_hz,
+      .mem_block_symbols = RMT_BLOCK_SYMBOLS_NUM,
+      .flags.invert_in = config->invert_in};
+  ESP_ERROR_CHECK(
+      rmt_new_rx_channel(&rmt_rx_chan_config, &ir_nec->rmt_rx_chan));
 
-    ir_nec_encoder_config_t nec_encoder_cfg = {
-        .resolution = config->resolution_hz,
-    };
-    ESP_ERROR_CHECK(rmt_new_ir_nec_encoder(&nec_encoder_cfg, &ir_nec->nec_encoder));
-    ESP_ERROR_CHECK(rmt_enable(ir_nec->rmt_rx_chan));
-    ESP_LOGI(IR_NEC_TAG, "ENABLED RX CHAN");
-    ESP_ERROR_CHECK(rmt_enable(ir_nec->rmt_tx_chan));
-    
-	return ESP_OK;
+  rmt_rx_event_callbacks_t cbs = {
+      .on_recv_done = rmt_rx_done_callback,
+  };
+  ESP_ERROR_CHECK(rmt_rx_register_event_callbacks(ir_nec->rmt_rx_chan, &cbs,
+                                                  &ir_nec->rmt_rx_queue));
+  ESP_ERROR_CHECK(rmt_enable(ir_nec->rmt_rx_chan));
+  ESP_LOGI(IR_NEC_TAG, "Done creating rx channel.");
+
+  rmt_tx_channel_config_t tx_channel_cfg = {
+      .clk_src = RMT_CLK_SRC_DEFAULT,
+      .resolution_hz = config->resolution_hz,
+      .mem_block_symbols = RMT_BLOCK_SYMBOLS_NUM,
+      .trans_queue_depth = 4,
+      .gpio_num = config->tx_gpio,
+  };
+  ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_channel_cfg, &ir_nec->rmt_tx_chan));
+  ESP_ERROR_CHECK(
+      rmt_apply_carrier(ir_nec->rmt_tx_chan, &config->tx_carrier_config));
+
+  ir_nec_encoder_config_t nec_encoder_cfg = {
+      .resolution = config->resolution_hz,
+  };
+  ESP_ERROR_CHECK(
+      rmt_new_ir_nec_encoder(&nec_encoder_cfg, &ir_nec->nec_encoder));
+  ESP_ERROR_CHECK(rmt_enable(ir_nec->rmt_tx_chan));
+  ESP_LOGI(IR_NEC_TAG, "Done creating tx channel.");
+  ESP_LOGI(IR_NEC_TAG, "Done initializing module.");
+
+  return ESP_OK;
 }
 
 esp_err_t ir_nec_tx_run(task_arg_t *task_arg) {
-	ir_nec_t *ir_nec = task_arg->dev_ptr;
-	if (ir_nec->tx_running || !ir_nec->initialized) return ESP_ERR_INVALID_STATE;
-	xTaskCreate(ir_nec_tx_task, "ir_nec_tx_task", 3072, task_arg, 12, &ir_nec->tx_task_handle);
-	ir_nec->tx_running = true;
-	return ESP_OK;
+  ESP_LOGI(IR_NEC_TAG, "Running tx task...");
+  ir_nec_t *ir_nec = task_arg->dev_ptr;
+  if (ir_nec->tx_running || !ir_nec->initialized)
+    return ESP_ERR_INVALID_STATE;
+  xTaskCreate(ir_nec_tx_task, "ir_nec_tx_task", 3072, task_arg, 12,
+              &ir_nec->tx_task_handle);
+  ir_nec->tx_running = true;
+  ESP_LOGI(IR_NEC_TAG, "Tx task is running.");
+  return ESP_OK;
 }
 
 esp_err_t ir_nec_rx_run(task_arg_t *task_arg) {
-	ir_nec_t *ir_nec = task_arg->dev_ptr;
-	if (ir_nec->rx_running || !ir_nec->initialized) return ESP_ERR_INVALID_STATE;
-	xTaskCreate(ir_nec_rx_task, "ir_nec_rx_task", 3072, task_arg, 12, &ir_nec->rx_task_handle);
-	ir_nec->rx_running = true;
-	return ESP_OK;
+  ir_nec_t *ir_nec = task_arg->dev_ptr;
+  if (ir_nec->rx_running || !ir_nec->initialized)
+    return ESP_ERR_INVALID_STATE;
+  xTaskCreate(ir_nec_rx_task, "ir_nec_rx_task", 3072, task_arg, 12,
+              &ir_nec->rx_task_handle);
+  ir_nec->rx_running = true;
+  ESP_LOGI(IR_NEC_TAG, "Rx task is running.");
+  return ESP_OK;
 }
 
 esp_err_t ir_nec_tx_stop(ir_nec_t *ir_nec) {
-	if (ir_nec->tx_running && ir_nec->initialized) {
-		vTaskSuspend(ir_nec->tx_task_handle);
-		ir_nec->tx_running = false;
-		reset_ring_buffer(ir_nec->tx_interm_buf);
-		return ESP_OK;		
-	}
-	return ESP_ERR_INVALID_STATE;
+  ESP_LOGI(IR_NEC_TAG, "Stopping tx task...");
+  if (ir_nec->tx_running && ir_nec->initialized) {
+    vTaskSuspend(ir_nec->tx_task_handle);
+    ir_nec->tx_running = false;
+    reset_ring_buffer(ir_nec->tx_interm_buf);
+    ESP_LOGI(IR_NEC_TAG, "Tx task is stopped.");
+    return ESP_OK;
+  }
+  return ESP_ERR_INVALID_STATE;
 }
 
 esp_err_t ir_nec_rx_stop(ir_nec_t *ir_nec) {
-	if (ir_nec->rx_running && ir_nec->initialized) {
-		vTaskSuspend(ir_nec->rx_task_handle);
-		ir_nec->rx_running = false;
-		reset_ring_buffer(ir_nec->rx_interm_buf);
-        xQueueReset(ir_nec->rmt_rx_queue);
-		return ESP_OK;
-	}
-	return ESP_ERR_INVALID_STATE;
+  ESP_LOGI(IR_NEC_TAG, "Stopping tx task...");
+  if (ir_nec->rx_running && ir_nec->initialized) {
+    vTaskSuspend(ir_nec->rx_task_handle);
+    ir_nec->rx_running = false;
+    reset_ring_buffer(ir_nec->rx_interm_buf);
+    xQueueReset(ir_nec->rmt_rx_queue);
+    ESP_LOGI(IR_NEC_TAG, "Rx task is stopped.");
+    return ESP_OK;
+  }
+  return ESP_ERR_INVALID_STATE;
 }
 
-size_t ir_nec_read(ir_nec_t *ir_nec, nec_scan_code_t* buf) {
-	size_t res_size;
-	void *res = xRingbufferReceive(ir_nec->rx_interm_buf, &res_size, 10);
-	if (res == NULL) {
-		ESP_LOGE(IR_NEC_TAG, "RX: Failed to receive data from intermediate buffer");
-		return 0;
-	}
-	memcpy(buf, res, res_size);
-	vRingbufferReturnItem(ir_nec->rx_interm_buf, res);
-	return res_size;
+size_t ir_nec_read(ir_nec_t *ir_nec, nec_scan_code_t *buf) {
+  size_t res_size;
+  void *res = xRingbufferReceive(ir_nec->rx_interm_buf, &res_size, 10);
+  if (res == NULL) {
+    ESP_LOGE(IR_NEC_TAG,
+             "RX: Failed to receive data from intermediate buffer.");
+    return 0;
+  }
+  memcpy(buf, res, res_size);
+  vRingbufferReturnItem(ir_nec->rx_interm_buf, res);
+  return res_size;
 }
 
-void ir_nec_write(ir_nec_t *ir_nec, nec_scan_code_t* buf) {
-	if (xRingbufferSend(ir_nec->tx_interm_buf, buf, sizeof(nec_scan_code_t), 10) == pdFALSE) {
-		ESP_LOGE(IR_NEC_TAG, "TX: Failed to send data to intermediate buffer");
-		return;
-	}
+void ir_nec_write(ir_nec_t *ir_nec, nec_scan_code_t *buf) {
+  if (xRingbufferSend(ir_nec->tx_interm_buf, buf, sizeof(nec_scan_code_t),
+                      10) == pdFALSE) {
+    ESP_LOGE(IR_NEC_TAG, "TX: Failed to send data to intermediate buffer.");
+    return;
+  }
 }
-
 
 esp_err_t ir_nec_deinit(ir_nec_t *ir_nec) {
-	vRingbufferDelete(ir_nec->rx_interm_buf);
-	vRingbufferDelete(ir_nec->tx_interm_buf);
-    vQueueDelete(ir_nec->rmt_rx_queue);
-	ESP_ERROR_CHECK(rmt_disable(ir_nec->rmt_rx_chan));
-    ESP_ERROR_CHECK(rmt_disable(ir_nec->rmt_tx_chan));
-    rmt_del_channel(ir_nec->rmt_rx_chan);
-    rmt_del_channel(ir_nec->rmt_tx_chan);
-    rmt_del_encoder(ir_nec->nec_encoder);
-	ir_nec->rx_running = false;
-	ir_nec->tx_running = false;
-	ir_nec->initialized = false;
-	return ESP_OK;
+  ESP_LOGI(IR_NEC_TAG, "Deinitializing module...");
+  vRingbufferDelete(ir_nec->rx_interm_buf);
+  vRingbufferDelete(ir_nec->tx_interm_buf);
+  vQueueDelete(ir_nec->rmt_rx_queue);
+  ESP_ERROR_CHECK(rmt_disable(ir_nec->rmt_rx_chan));
+  ESP_ERROR_CHECK(rmt_disable(ir_nec->rmt_tx_chan));
+  rmt_del_channel(ir_nec->rmt_rx_chan);
+  rmt_del_channel(ir_nec->rmt_tx_chan);
+  rmt_del_encoder(ir_nec->nec_encoder);
+  ir_nec->rx_running = false;
+  ir_nec->tx_running = false;
+  ir_nec->initialized = false;
+  ESP_LOGI(IR_NEC_TAG, "Done deinitializing module...");
+  return ESP_OK;
 }
--- components/ir_nec/ir_nec_parser.c
+++ components/ir_nec/ir_nec_parser.c
@@ -4,169 +4,176 @@
  * SPDX-License-Identifier: Unlicense
  */
 #include "ir_nec_parser.h"
+#include "common.h"
 
 /**
  * @brief Check whether a duration is within expected range
  */
-static inline bool nec_check_in_range(uint32_t signal_duration, uint32_t spec_duration)
-{
-    return (signal_duration < (spec_duration + EXAMPLE_IR_NEC_DECODE_MARGIN)) &&
-           (signal_duration > (spec_duration - EXAMPLE_IR_NEC_DECODE_MARGIN));
+static inline bool nec_check_in_range(uint32_t signal_duration,
+                                      uint32_t spec_duration) {
+  return (signal_duration < (spec_duration + EXAMPLE_IR_NEC_DECODE_MARGIN)) &&
+         (signal_duration > (spec_duration - EXAMPLE_IR_NEC_DECODE_MARGIN));
 }
 
-bool nec_parse_logic0(rmt_symbol_word_t *rmt_nec_symbols)
-{
-    return nec_check_in_range(rmt_nec_symbols->duration0, NEC_PAYLOAD_ZERO_DURATION_0) &&
-           nec_check_in_range(rmt_nec_symbols->duration1, NEC_PAYLOAD_ZERO_DURATION_1);
+bool nec_parse_logic0(rmt_symbol_word_t *rmt_nec_symbols) {
+  return nec_check_in_range(rmt_nec_symbols->duration0,
+                            NEC_PAYLOAD_ZERO_DURATION_0) &&
+         nec_check_in_range(rmt_nec_symbols->duration1,
+                            NEC_PAYLOAD_ZERO_DURATION_1);
 }
 
-bool nec_parse_logic1(rmt_symbol_word_t *rmt_nec_symbols)
-{
-    return nec_check_in_range(rmt_nec_symbols->duration0, NEC_PAYLOAD_ONE_DURATION_0) &&
-           nec_check_in_range(rmt_nec_symbols->duration1, NEC_PAYLOAD_ONE_DURATION_1);
+bool nec_parse_logic1(rmt_symbol_word_t *rmt_nec_symbols) {
+  return nec_check_in_range(rmt_nec_symbols->duration0,
+                            NEC_PAYLOAD_ONE_DURATION_0) &&
+         nec_check_in_range(rmt_nec_symbols->duration1,
+                            NEC_PAYLOAD_ONE_DURATION_1);
 }
 
 /*static inline void nec_set_bit(uint8_t *byte, int bit_index, bool lsb)
 {
     if (lsb) {
-		*byte |= (1 << bit_index);
+                *byte |= (1 << bit_index);
     } else {
         *byte |= (1 << (7 - bit_index));
     }
 }*/
 
-static uint8_t reverse_byte(uint8_t x)
-{
-    x = (x & 0xF0) >> 4 | (x & 0x0F) << 4;
-    x = (x & 0xCC) >> 2 | (x & 0x33) << 2;
-    x = (x & 0xAA) >> 1 | (x & 0x55) << 1;
-    return x;
+static uint8_t reverse_byte(uint8_t x) {
+  x = (x & 0xF0) >> 4 | (x & 0x0F) << 4;
+  x = (x & 0xCC) >> 2 | (x & 0x33) << 2;
+  x = (x & 0xAA) >> 1 | (x & 0x55) << 1;
+  return x;
 }
 
-bool nec_parse_frame(rmt_symbol_word_t *rmt_nec_symbols, bool err_cor, bool lsb_format, nec_scan_code_t *ret)
-{
-    if (!rmt_nec_symbols || !ret) return false;
-
-    rmt_symbol_word_t *cur = rmt_nec_symbols;
-
-    // Check leader
-    bool leader_ok =
-    (nec_check_in_range(cur->duration0, NEC_LEADING_CODE_DURATION_0) &&
-     nec_check_in_range(cur->duration1, NEC_LEADING_CODE_DURATION_1)) ||
-
-    (nec_check_in_range(cur->duration0, NECX_LEADING_CODE_DURATION_0) &&
-     nec_check_in_range(cur->duration1, NECX_LEADING_CODE_DURATION_1));
-
-	if (!leader_ok) {
-	    return false;
-	}
-
-    cur++;
-
-    uint8_t addr_l = 0;
-    uint8_t addr_h = 0;
-    uint8_t cmd_l  = 0;
-    uint8_t cmd_h  = 0;
-
-    // Decode 16-bit address (LSB-first per byte)
-    for (int bit = 0; bit < 8; bit++) {
-        if (nec_parse_logic1(cur)) {
-            addr_l |= (1 << bit);
-        } else if (!nec_parse_logic0(cur)) {
-            return false;
-        }
-        cur++;
-    }
-
-    for (int bit = 0; bit < 8; bit++) {
-        if (nec_parse_logic1(cur)) {
-            addr_h |= (1 << bit);
-        } else if (!nec_parse_logic0(cur)) {
-            return false;
-        }
-        cur++;
-    }
-
-    //Decode 16-bit command
-    for (int bit = 0; bit < 8; bit++) {
-        if (nec_parse_logic1(cur)) {
-            cmd_l |= (1 << bit);
-        } else if (!nec_parse_logic0(cur)) {
-            return false;
-        }
-        cur++;
-    }
-
-    for (int bit = 0; bit < 8; bit++) {
-        if (nec_parse_logic1(cur)) {
-            cmd_h |= (1 << bit);
-        } else if (!nec_parse_logic0(cur)) {
-            return false;
-        }
-        cur++;
-    }
-
-    // Standard NEC: command is inverted in extended frame
-    // or address/command checksum depending on variant
-
-    
-    if (err_cor) {
-		// Validate command
-		if ((uint8_t)(cmd_l ^ cmd_h) != 0xFF) {
-		    return false;
-		}
-		
-		// Validate address (classic NEC, not extended)
-		if ((uint8_t)(addr_l ^ addr_h) != 0xFF) {
-		    return false;
-		}
-	}
-	
-	
-    
-	if (!lsb_format) {
-	    addr_l = reverse_byte(addr_l);
-	    addr_h = reverse_byte(addr_h);
-	    cmd_l  = reverse_byte(cmd_l);
-	    cmd_h  = reverse_byte(cmd_h);
-	}
-
-
-    // Save result
-    ret->address = ((uint16_t)addr_h << 8) | addr_l;
-	ret->command = ((uint16_t)cmd_h << 8) | cmd_l;
-
-    return true;
-}
-
-bool nec_parse_frame_repeat(rmt_symbol_word_t *rmt_nec_symbols)
-{
-    return nec_check_in_range(rmt_nec_symbols->duration0, NEC_REPEAT_CODE_DURATION_0) &&
-           nec_check_in_range(rmt_nec_symbols->duration1, NEC_REPEAT_CODE_DURATION_1);
-}
-
-bool parse_received_symbols_to_nec(rmt_symbol_word_t *rmt_nec_symbols, size_t num_symbols, bool err_cor, nec_scan_code_t *ret) {
-	 printf("NEC frame start---\r\n");
-    for (size_t i = 0; i < num_symbols; i++) {
-        printf("{%d:%d},{%d:%d}\r\n", rmt_nec_symbols[i].level0, rmt_nec_symbols[i].duration0,
-               rmt_nec_symbols[i].level1, rmt_nec_symbols[i].duration1);
-    }
-    printf("---NEC frame end: ");
-    // decode RMT symbols
-    switch (num_symbols) {
-    case 34: // NEC normal frame
-        if (nec_parse_frame(rmt_nec_symbols, err_cor, 1, ret)) {
-			return true;
-        }
-        break;
-    case 2: // NEC repeat frame
-        if (nec_parse_frame_repeat(rmt_nec_symbols)) {
-            printf("Repeat\n");
-        }
-        break;
-    default:
-        printf("Unknown NEC frame\r\n\r\n");
-        break;
-    }
+bool nec_parse_frame(rmt_symbol_word_t *rmt_nec_symbols, bool err_cor,
+                     bool lsb_format, nec_scan_code_t *ret) {
+  if (!rmt_nec_symbols || !ret)
     return false;
+
+  rmt_symbol_word_t *cur = rmt_nec_symbols;
+
+  // Check leader
+  bool leader_ok =
+      (nec_check_in_range(cur->duration0, NEC_LEADING_CODE_DURATION_0) &&
+       nec_check_in_range(cur->duration1, NEC_LEADING_CODE_DURATION_1)) ||
+
+      (nec_check_in_range(cur->duration0, NECX_LEADING_CODE_DURATION_0) &&
+       nec_check_in_range(cur->duration1, NECX_LEADING_CODE_DURATION_1));
+
+  if (!leader_ok) {
+    return false;
+  }
+
+  cur++;
+
+  uint8_t addr_l = 0;
+  uint8_t addr_h = 0;
+  uint8_t cmd_l = 0;
+  uint8_t cmd_h = 0;
+
+  // Decode 16-bit address (LSB-first per byte)
+  for (int bit = 0; bit < 8; bit++) {
+    if (nec_parse_logic1(cur)) {
+      addr_l |= (1 << bit);
+    } else if (!nec_parse_logic0(cur)) {
+      return false;
+    }
+    cur++;
+  }
+
+  for (int bit = 0; bit < 8; bit++) {
+    if (nec_parse_logic1(cur)) {
+      addr_h |= (1 << bit);
+    } else if (!nec_parse_logic0(cur)) {
+      return false;
+    }
+    cur++;
+  }
+
+  // Decode 16-bit command
+  for (int bit = 0; bit < 8; bit++) {
+    if (nec_parse_logic1(cur)) {
+      cmd_l |= (1 << bit);
+    } else if (!nec_parse_logic0(cur)) {
+      return false;
+    }
+    cur++;
+  }
+
+  for (int bit = 0; bit < 8; bit++) {
+    if (nec_parse_logic1(cur)) {
+      cmd_h |= (1 << bit);
+    } else if (!nec_parse_logic0(cur)) {
+      return false;
+    }
+    cur++;
+  }
+
+  // Standard NEC: command is inverted in extended frame
+  // or address/command checksum depending on variant
+
+  if (err_cor) {
+    // Validate command
+    if ((uint8_t)(cmd_l ^ cmd_h) != 0xFF) {
+      return false;
+    }
+
+    // Validate address (classic NEC, not extended)
+    if ((uint8_t)(addr_l ^ addr_h) != 0xFF) {
+      return false;
+    }
+  }
+
+  if (!lsb_format) {
+    addr_l = reverse_byte(addr_l);
+    addr_h = reverse_byte(addr_h);
+    cmd_l = reverse_byte(cmd_l);
+    cmd_h = reverse_byte(cmd_h);
+  }
+
+  // Save result
+  ret->address = ((uint16_t)addr_h << 8) | addr_l;
+  ret->command = ((uint16_t)cmd_h << 8) | cmd_l;
+
+  return true;
+}
+
+bool nec_parse_frame_repeat(rmt_symbol_word_t *rmt_nec_symbols) {
+  return nec_check_in_range(rmt_nec_symbols->duration0,
+                            NEC_REPEAT_CODE_DURATION_0) &&
+         nec_check_in_range(rmt_nec_symbols->duration1,
+                            NEC_REPEAT_CODE_DURATION_1);
+}
+
+bool parse_received_symbols_to_nec(rmt_symbol_word_t *rmt_nec_symbols,
+                                   size_t num_symbols, bool err_cor,
+                                   nec_scan_code_t *ret) {
+  if (g_debug_enabled) {
+    printf("NEC frame start---\r\n");
+    for (size_t i = 0; i < num_symbols; i++) {
+      printf("{%d:%d},{%d:%d}\r\n", rmt_nec_symbols[i].level0,
+             rmt_nec_symbols[i].duration0, rmt_nec_symbols[i].level1,
+             rmt_nec_symbols[i].duration1);
+    }
+    printf("---NEC frame end ---\r\n");
+  }
+  // decode RMT symbols
+  switch (num_symbols) {
+  case 34: // NEC normal frame
+    if (nec_parse_frame(rmt_nec_symbols, err_cor, 1, ret)) {
+      return true;
+    }
+    break;
+  case 2: // NEC repeat frame
+    if (nec_parse_frame_repeat(rmt_nec_symbols)) {
+      if (g_debug_enabled)
+        printf("Repeat\n");
+    }
+    break;
+  default:
+    if (g_debug_enabled)
+      printf("Unknown NEC frame\r\n\r\n");
+    break;
+  }
+  return false;
 }
